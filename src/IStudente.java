75279a167afdc68c7e5336d4365fbc1ef2fedfbf
alex <aless.distef@gmail.com>
2024-11-23 19:30:05 +0100

    Aggiunta interfaccia IStudente

---
+++ src/IStudente.java
@@ -0,0 +1,15 @@
+public interface IStudente {
+    // Metodi get
+    String getNome();
+    String getCognome();
+    String getCodiceFiscale();
+    int getMatricola();
+    String getEmail();
+
+    // Metodi set
+    void setNome(String nome);
+    void setCognome(String cognome);
+    void setCodiceFiscale(String codiceFiscale);
+    void setMatricola(int matricola);
+    void setEmail(String email);
+}
