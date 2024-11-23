9ef4f74fca1ce1f4026e7adfc11b9a2b9f9da3d7
alex <aless.distef@gmail.com>
2024-11-23 19:34:19 +0100

    Aggiunto un oggetto Prenotazione che rappresenta una prenotazione.

---
+++ src/Prenotazione.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Prenotazione implements Serializable {
+    private int matricola;
+    private long codiceEsame;
+
+    public Prenotazione(int matricola, long codiceEsame) {
+        this.matricola = matricola;
+        this.codiceEsame = codiceEsame;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    public long getCodiceEsame() {
+        return codiceEsame;
+    }
+}
